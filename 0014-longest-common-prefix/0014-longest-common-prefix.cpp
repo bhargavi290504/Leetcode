9bfb5d4607641da4405b2a6f3edc4b8febd43c75
bhargavi290504 <144715760+bhargavi290504@users.noreply.github.com>
2024-02-06 18:46:48 +0530

    Time: 7 ms (21.93%), Space: 10.8 MB (26.51%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {   
+        string ans="";     
+        int n=strs.size();
+        for(int i=0;i<strs[0].length() ;i++)
+        {
+            char ch=strs[0][i];
+            bool match=true;
+            for(int j=1;j<n;j++)
+            {
+                if(strs[j].length()<i || strs[j][i]!=ch) 
+                {
+                    match =false ;
+                    break;
+                }
+            }  
+            if(match==false)
+            {
+                break;
+            }  
+            else {
+                ans.push_back(ch);
+            }
+        }  
+        return ans;
+        
+    }
+};
