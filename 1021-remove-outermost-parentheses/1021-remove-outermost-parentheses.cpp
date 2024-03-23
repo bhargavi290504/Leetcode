212abef88cfb809db349aeca6b1b81416d70ffdd
bhargavi290504 <144715760+bhargavi290504@users.noreply.github.com>
2024-03-23 09:42:19 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (80.13%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -4,11 +4,15 @@ public:
         string res;
         int opened = 0;
         for (char c : s) {
-            if (c == '(' && opened++ > 0) res += c;
-            if (c == ')' && opened-- > 1) res += c;
+            if (c == '(') {
+                if (opened > 0) res += c;
+                opened++;
+            } else if (c == ')') {
+                opened--;
+                if (opened > 0) res += c;
+            }
         }
         return res;
- 
         
     }
 };
