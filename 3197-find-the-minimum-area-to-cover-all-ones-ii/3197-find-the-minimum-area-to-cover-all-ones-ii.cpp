a2ee73718ba79ba20e8faaf2a92dc499e9a667e9
bhargavi290504 <144715760+bhargavi290504@users.noreply.github.com>
2024-06-24 15:56:01 +0530

    Time: 84 ms (100.00%), Space: 24 MB (100.00%) - LeetHub

---
+++ 3197-find-the-minimum-area-to-cover-all-ones-ii/3197-find-the-minimum-area-to-cover-all-ones-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int min1(vector<vector<int>>& g, int b, int l, int t, int r) { // bottom, left, top, right
+    int _b = 30, _l = 30, _t = 0, _r = 0;
+    for (int i = b; i < t; ++i)
+        for (int j = l; j < r; ++j)
+            if (g[i][j]) {
+                _b = min(_b, i);
+                _l = min(_l, j);
+                _t = max(_t, i);
+                _r = max(_r, j);                    
+            }
+    return (_t - _b + 1) * (_r - _l + 1);
+}
+int min2v(vector<vector<int>>& g, int b, int l, int t, int r) {
+    int res = INT_MAX;
+    for (int i = b + 1; i <= t; ++i)
+        res = min(res, min1(g, b, l, i, r) + min1(g, i, l, t, r));
+    return res;
+}
+int min2h(vector<vector<int>>& g, int b, int l, int t, int r) {
+    int res = INT_MAX;
+    for (int j = l + 1; j <= r; ++j)
+        res = min(res, min1(g, b, l, t, j) + min1(g, b, j, t, r));
+    return res;
+}    
+int minimumSum(vector<vector<int>>& g) {
+    int res = INT_MAX, m = g.size(), n = g[0].size();
+    for (int i = 1; i <= m; ++i)
+        res = min({res, min1(g, 0, 0, i, n) + min2h(g, i, 0, m, n), 
+                        min1(g, i, 0, m, n) + min2h(g, 0, 0, i, n), 
+                        min1(g, i, 0, m, n) + min2v(g, 0, 0, i, n)});
+    for (int j = 1; j <= n; ++j)
+        res = min({res, min1(g, 0, 0, m, j) + min2v(g, 0, j, m, n), 
+                        min1(g, 0, j, m, n) + min2v(g, 0, 0, m, j), 
+                        min1(g, 0, j, m, n) + min2h(g, 0, 0, m, j)});           
+    return res;
+}
+};
