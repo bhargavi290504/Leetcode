b699f4daf338ff8c33786625a30cb30ef454a2e2
Sreekantha Bhargavi <144715760+bhargavi290504@users.noreply.github.com>
2024-11-13 11:55:32 +0530

    Time: 27 ms (94.05%), Space: 60.5 MB (18.39%) - LeetHub

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.cpp
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    long long countFairPairs(vector<int>& nums, int lower, int upper) {  
-        sort(nums.begin(), nums.end());
+    long long countLess(vector<int>& nums, int val) {
+    long long res = 0;
+    for (int i = 0, j = nums.size() - 1; i < j; ++i) {
+        while(i < j && nums[i] + nums[j] > val)
+            --j;
+        res += j - i;
+    }        
+    return res;
+}
+long long countFairPairs(vector<int>& nums, int lower, int upper) {
+    sort(begin(nums), end(nums));
+    return countLess(nums, upper) - countLess(nums, lower - 1);
+}
 
-        long long fairPairCount = 0;
-        int n = nums.size();
-
-        for (int i = 0; i < n - 1; i++) {
-            int jLower = lower_bound(nums.begin() + i + 1, nums.end(), lower - nums[i]) - nums.begin();
-            int jUpper = upper_bound(nums.begin() + i + 1, nums.end(), upper - nums[i]) - nums.begin();
-            fairPairCount += (jUpper - jLower);
-        }
-
-        return fairPairCount;
-    }
 };
